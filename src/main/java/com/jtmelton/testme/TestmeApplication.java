fd5879d4dd66885216749682a9ff01bdaaf70ec1
jtmelton <jtmelton@gmail.com>
2017-06-25 22:59:17 -0400

    Initial Commit.

---
+++ src/main/java/com/jtmelton/testme/TestmeApplication.java
@@ -0,0 +1,12 @@
+package com.jtmelton.testme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmeApplication.class, args);
+	}
+}
