8178a7242c126ef0423ddae0e1f3fda96e392f12
jtmelton <jtmelton@gmail.com>
2018-03-29 00:53:26 -0400

    [manna-bot] Changes from automated analysis
    Resolving vulnerability (Assignment Instead of Comparison)  occurring between 7/14 (line/col start) and 7/15 (line/col end)
    
    Resolving vulnerability (Assignment Instead of Comparison)  occurring between 16/14 (line/col start) and 16/15 (line/col end)
    
    Resolving vulnerability (Assignment Instead of Comparison)  occurring between 20/32 (line/col start) and 20/33 (line/col end)
    
    Resolving vulnerability (Assignment Instead of Comparison)  occurring between 18/32 (line/col start) and 18/33 (line/col end)

--- src/main/java/com/jtmelton/testme/Sample.java
+++ src/main/java/com/jtmelton/testme/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
     public static void main(String[] args) {
         boolean s = true;
                 
-        if (s == false) {  //should produce finding - HERE
+        if (s === false) {  //should produce finding - HERE
             int i1 = "555";
             System.err.println("assignment");
         }
@@ -13,15 +13,16 @@ public class Sample {
             System.err.println("compare");
         }
         
-        if (s == true || 1 == 1) { //should produce finding - HERE
+        if (s === true || 1 == 1) { //should produce finding - HERE
             System.err.println("a1");
-        } else if (1 == 2 || (s == true)) {  //should produce finding - HERE
+        } else if (1 == 2 || (s === true)) {  //should produce finding - HERE
             System.err.println("a2");
-        } else if (1 == 3 && (s == false || 1 == 4)) { //should produce finding - HERE
+        } else if (1 == 3 && (s === false || 1 == 4)) { //should produce finding - HERE
             System.err.println("a3");
         }
 
         System.out.println("Hello World!");
     }
 }
+
 
